de67aeaf873371a6d04a724ce1cd1ee80affe10d
abahgatabdelhamed <ahmedbahgat1606@gmail.com>
2024-11-01 21:04:59 +0200

    login and register and localization

--- src/Comopnents/header/header.component.ts
+++ src/Comopnents/header/header.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../Services/Language/language.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive ,TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+constructor(private lanSer:LanguageService){
+
+}
+switchLanguage(lang:string){
+  this.lanSer.changeLang(lang);
+}
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../Comopnents/header/header.component';
 import { HomeComponent } from '../Comopnents/home/home.component';
 import { FooterComponent } from '../Comopnents/footer/footer.component';
+import {TranslateModule} from "@ngx-translate/core";   // <--- standalone only
+import {TranslateService} from "@ngx-translate/core";
+
+import { BehaviorSubject, Observable } from 'rxjs';
+import { LanguageService } from '../Services/Language/language.service';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HomeComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, HomeComponent, FooterComponent, RouterOutlet,TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'WalMart';
+  lang:string="ar"
+  
+  constructor(private translate: TranslateService , private langSer:LanguageService) {
+    this.translate.use(this.lang);
+    
+  }
+  ngOnInit(): void {
+    this.langSer.getLangugae().subscribe({
+      next: (res) => {
+        this.lang = res;
+      },
+      error: (err) => {
+        console.error("Error fetching language:", err);
+      }
+    });
+  }
+
 }
