bac579ac1d443bc5c40577c78ac05aa6c37db92f
omeragamy20 <153401997+omeragamy20@users.noreply.github.com>
2024-11-19 15:59:34 +0200

    V1.1

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,8 @@ export const environment = {
 // baseURL:'https://localhost:7016/api',
 //   baseuRL: 'https://localhost:7016/api',
 //   url:"https://localhost:7028"
-  baseURL:'https://e-walmartapi.runasp.net/api',
-  baseuRL: 'https://e-walmartapi.runasp.net/api',
+  baseURL:'http://e-walmartapi.runasp.net/api',
+  baseuRL: 'http://e-walmartapi.runasp.net/api',
   url:"https://e-walmart.runasp.net/"
 
 };
