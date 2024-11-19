911657d0f7f5c5251c1fed5cd2b13b89fe07b41f
omeragamy20 <153401997+omeragamy20@users.noreply.github.com>
2024-11-19 10:44:58 +0200

    verson-1

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,8 @@
 export const environment = {
- baseURL:'https://localhost:7016/api',
-  baseuRL: 'https://localhost:7016/api',
+// baseURL:'https://localhost:7016/api',
+  // baseuRL: 'https://localhost:7016/api',
+  baseURL:'https://e-walmartapi.runasp.net/api',
+  baseuRL: 'https://e-walmartapi.runasp.net/api',
   url:"https://localhost:7028"
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  baseURL:'https://localhost:7016/api',
-  baseuRL: 'https://localhost:7016/api',
+  // baseURL:'https://localhost:7016/api',
+  // baseuRL: 'https://localhost:7016/api',
+  baseURL:'https://e-walmartapi.runasp.net/api',
+  baseuRL: 'https://e-walmartapi.runasp.net/api',
   url:"https://localhost:7028"
 };
